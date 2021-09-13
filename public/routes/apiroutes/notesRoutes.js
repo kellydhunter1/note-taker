74d2c78fe20d2d4c111d1e13dc81f3fc616f00f9
kellydhunter1 <kellydhunter1@gmail.com>
2021-09-13 11:01:29 -0400

    starter files

--- public/routes/apiroutes/notesRoutes.js
+++ public/routes/apiroutes/notesRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
+    notes = filterByQuery(req.query, notes);
     res.json(notes);
     console.log(notes);
 });
